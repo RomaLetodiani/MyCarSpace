39f2d2488b1db64c6efe172f184024e808bb14a0
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-09 22:44:16 +0400

    Tailwind

--- Frontend/src/router/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-const PrivateRoute = () => {
-  // Add your authentication logic here
-  const isAuthenticated = false
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />
-}
-
-export default PrivateRoute
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#042890',
+        secondary: '#0A6FA0',
+        purple: '#84A3FF',
+      },
+
+      gradientColorStops: {
+        oceanBlue: '#042890',
+        skyBlue: '#0A6FA0',
+      },
+    },
+  },
+  plugins: [],
+}
