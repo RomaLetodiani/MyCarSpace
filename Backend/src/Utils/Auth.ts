34a0b11824349044af31059c9d87741063d2714f
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-09 14:44:07 +0400

    Seed Admin

--- Backend/src/Utils/Auth.ts
+++ Backend/src/Utils/Auth.ts
@@ -1,8 +1,16 @@
 import jwt from "jsonwebtoken"
-import { Response } from "express"
 import bcrypt from "bcrypt"
+import { Role, User } from "../Models/User.Model"
 
-const generateTokens = (res: Response, user: any) => {
+export interface decodedUser {
+  username: string
+  email: string
+  role: Role
+  iat: number
+  exp: number
+}
+
+const generateTokens = (user: { username: string; email: string; role: Role }) => {
   const jwtSecret = process.env.JWT_SECRET_KEY || ""
   const accessToken = jwt.sign({ ...user }, jwtSecret, { expiresIn: "1h" })
   const refreshToken = jwt.sign({ ...user }, jwtSecret, { expiresIn: "1w" })
@@ -17,7 +25,32 @@ const hashPassword = async (password: string) => {
 
 const verifyToken = async (token: string) => {
   const jwtSecret = process.env.JWT_SECRET_KEY || ""
-  return jwt.verify(token, jwtSecret)
+  return jwt.verify(token, jwtSecret) as decodedUser
 }
 
-export { generateTokens, verifyToken, hashPassword }
+const decodeToken = (token: string) => {
+  return jwt.decode(token) as { username: string; email: string; role: Role }
+}
+
+const seedAdmin = async () => {
+  const adminUser = await User.findOne({ role: "admin" })
+
+  if (adminUser) {
+    console.log("🚀 ~ admin user already Created")
+    return
+  }
+
+  const admin = new User({
+    username: process.env.ADMIN_USERNAME,
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+    role: Role.ADMIN,
+  })
+
+  await admin.save()
+  console.log("🚀 ~ admin user Created")
+
+  return
+}
+
+export { generateTokens, decodeToken, verifyToken, hashPassword, seedAdmin }
