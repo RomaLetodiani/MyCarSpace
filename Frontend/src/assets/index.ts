c92ad61294102efc81027c3aae7309106ae7ba6a
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-09 22:44:50 +0400

    Slider Images

---
+++ Frontend/src/assets/index.ts
@@ -0,0 +1,14 @@
+import logo from './Images/Logo.png'
+
+// Slider Images
+import slider1 from './Images/Slider/1.jpg'
+import slider2 from './Images/Slider/2.jpg'
+import slider3 from './Images/Slider/3.jpg'
+
+// Footer Images
+import hours24 from './Images/Footer/24hours.png'
+import invoice from './Images/Footer/invoice.png'
+import delivery from './Images/Footer/delivery.png'
+import cash from './Images/Footer/cash.png'
+
+export { logo, slider1, slider2, slider3, hours24, invoice, delivery, cash }
