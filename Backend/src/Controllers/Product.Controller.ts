a71356591055d504db7e63f287f531f846778299
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-15 01:23:59 +0400

    Update Product.Controller.ts

--- Backend/src/Controllers/Product.Controller.ts
+++ Backend/src/Controllers/Product.Controller.ts
@@ -1,25 +1,61 @@
 import { Request, Response } from "express"
 import asyncHandler from "express-async-handler"
+import ProductServices from "../Services/Product.Service"
 
 class ProductController {
-  // Reusable method for handling unimplemented endpoints
-  private handleNotImplemented = (req: Request, res: Response) => {
-    res.status(501).json({
-      message: "Not Implemented Yet",
-    })
+  private ProductServices: ProductServices
+  constructor() {
+    this.ProductServices = new ProductServices()
   }
 
-  getProducts = asyncHandler(this.handleNotImplemented)
+  getProducts = asyncHandler(async (req: Request, res: Response) => {
+    const params = req.query
+    const result = await this.ProductServices.findAll(params)
+    res.status(200).json(result)
+  })
 
-  getProduct = asyncHandler(this.handleNotImplemented)
+  getProduct = asyncHandler(async (req: Request, res: Response) => {
+    const id = req.params.id
 
-  createProduct = asyncHandler(this.handleNotImplemented)
+    const result = await this.ProductServices.findOne({ id })
+    res.status(200).json(result)
+  })
 
-  updateProduct = asyncHandler(this.handleNotImplemented)
+  createProduct = asyncHandler(async (req: Request, res: Response) => {
+    const productData = req.body
 
-  archiveProducts = asyncHandler(this.handleNotImplemented)
+    const result = await this.ProductServices.create(productData)
+    res.status(201).json(result)
+  })
 
-  deleteProducts = asyncHandler(this.handleNotImplemented)
+  updateProduct = asyncHandler(async (req: Request, res: Response) => {
+    const id = req.params.id
+    const productData = req.body
+
+    const result = await this.ProductServices.update({ id, productData })
+    res.status(200).json(result)
+  })
+
+  archiveProducts = asyncHandler(async (req: Request, res: Response) => {
+    const ids = req.body.ids
+
+    const result = await this.ProductServices.archive({ ids })
+    res.status(200).json(result)
+  })
+
+  restoreProducts = asyncHandler(async (req: Request, res: Response) => {
+    const ids = req.body.ids
+
+    const result = await this.ProductServices.restore({ ids })
+    res.status(200).json(result)
+  })
+
+  deleteProducts = asyncHandler(async (req: Request, res: Response) => {
+    const ids = req.body.ids
+
+    const result = await this.ProductServices.delete({ ids })
+    res.status(200).json(result)
+  })
 }
 
 export default new ProductController()
