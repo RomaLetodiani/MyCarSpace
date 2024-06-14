20c978df602d4289b558932e7e9d2f97ecd7f07f
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-14 19:04:42 +0400

    Category model

---
+++ Backend/src/Models/Category.Model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+export interface CategoryDocument extends Document {
+  name: string
+  isArchived: boolean
+}
+
+const categorySchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    isArchived: { type: Boolean, required: true, default: false },
+  },
+  { versionKey: false, timestamps: true },
+)
+
+categorySchema.pre("deleteMany", async function (next) {
+  const categoryId = this.getQuery()["_id"]
+  const productsCount = await mongoose.model("Product").countDocuments({ category: categoryId })
+  if (productsCount > 0) {
+    throw new Error("Cannot delete category with associated products")
+  }
+  next()
+})
+
+export const User = mongoose.model<CategoryDocument>("Category", categorySchema)
