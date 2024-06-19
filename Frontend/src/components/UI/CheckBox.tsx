b22878008796e68e6f2dd193ba34656e0cffd0c4
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-19 23:02:41 +0400

    CheckBox

---
+++ Frontend/src/components/UI/CheckBox.tsx
@@ -0,0 +1,47 @@
+import { InputHTMLAttributes } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface CheckBoxI extends InputHTMLAttributes<HTMLInputElement> {
+  id: string
+  checked: boolean
+  checkedText?: string
+  uncheckedText?: string
+  withText?: boolean
+  clickable?: boolean
+}
+
+const CheckBox = ({
+  id,
+  checked,
+  withText = false,
+  checkedText,
+  uncheckedText,
+  clickable = true,
+  ...rest
+}: CheckBoxI) => {
+  return (
+    <div className="flex items-center gap-2">
+      <label
+        className={twMerge('flex gap-2 items-center', clickable && 'pointer-events-auto')}
+        htmlFor={id}
+      >
+        <div
+          className={twMerge(
+            'flex justify-center pointer-events-none items-center w-5 h-5 border rounded-full cursor-pointer transition-colors duration-300 ease-in-out',
+            checked ? 'bg-primary' : 'bg-gray-200',
+          )}
+        >
+          <div
+            className={twMerge(
+              'w-2 h-2 rounded-full bg-white transform transition-transform duration-300 ease-in-out',
+            )}
+          ></div>
+        </div>
+        <p>{withText && (checked ? checkedText : uncheckedText)}</p>
+      </label>
+      <input {...rest} type="checkbox" className="hidden" id={id} />
+    </div>
+  )
+}
+
+export default CheckBox
