a6855bb4f941864da5dabf004528ed2cd76f3b99
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-19 09:59:22 +0400

    Remove images feature

--- Admin/src/components/ImagesToBase64Converter.tsx
+++ Admin/src/components/ImagesToBase64Converter.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 type Props = {
   handleChange: (base64Images: string[]) => void
@@ -8,6 +8,10 @@ type Props = {
 
 const ImageToBase64Converter = ({ handleChange, initialImages = [], id }: Props) => {
   const [base64Images, setBase64Images] = useState(initialImages)
+
+  useEffect(() => {
+    handleChange(base64Images.map((base64) => base64.split(',')[1]))
+  }, [base64Images])
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return
 
@@ -24,34 +28,46 @@ const ImageToBase64Converter = ({ handleChange, initialImages = [], id }: Props)
 
     Promise.all(promises)
       .then((base64Images) => {
-        setBase64Images(base64Images)
-        handleChange(base64Images.map((base64) => base64.split(',')[1]))
+        setBase64Images((prev) => [...prev, ...base64Images])
       })
       .catch((error) => {
         throw error
       })
   }
+
+  const handleRemoveImage = (index: number) => {
+    const updatedBase64Images = base64Images.filter((_, i) => i !== index)
+    setBase64Images(updatedBase64Images)
+  }
   return (
-    <label htmlFor={id}>
-      <div className="w-full flex flex-col gap-2 p-5 cursor-pointer rounded-xl border bg-white">
+    <div className="w-full flex flex-col gap-2 p-5 rounded-xl border bg-white">
+      <label htmlFor={id} className="cursor-pointer">
         <p>აირჩიეთ ფოტოები</p>
-        <input
-          id={id}
-          accept="image/png, image/jpeg, image/gif"
-          className="hidden"
-          type="file"
-          multiple
-          onChange={handleChangeInput}
-        />
-        {base64Images && (
-          <div className="flex flex-wrap gap-5">
-            {base64Images.map((base64, index) => (
-              <img key={index} src={base64} alt="base64" className="w-20 h-20 rounded-md" />
-            ))}
-          </div>
-        )}
-      </div>
-    </label>
+      </label>
+      <input
+        id={id}
+        accept="image/png, image/jpeg, image/gif"
+        className="hidden"
+        type="file"
+        multiple
+        onChange={handleChangeInput}
+      />
+      {base64Images && (
+        <div className="flex flex-wrap gap-5">
+          {base64Images.map((base64, index) => (
+            <div key={index} className="relative">
+              <img src={base64} alt="base64" className="w-20 h-20 rounded-md" />
+              <button
+                onClick={() => handleRemoveImage(index)}
+                className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center"
+              >
+                &times;
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
 
