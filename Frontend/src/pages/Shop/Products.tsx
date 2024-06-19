c38ff914f696d62b4595005ae2349ef1fba1a638
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-19 23:03:15 +0400

    Products

--- Frontend/src/pages/Shop/Products.tsx
+++ Frontend/src/pages/Shop/Products.tsx
@@ -1,17 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Pagination from '../../components/Pagination/Pagination'
+import Loading from '../Loading/Loading'
+import FilterStore from '../../Stores/Filter.Store'
+import ProductCard from './ProductCard'
+import useMediaQuery from '../../hooks/useMediaQuery'
+import { twMerge } from 'tailwind-merge'
 
 const Products = () => {
   const [page, setPage] = useState(1)
-  const [pageSize, setPageSize] = useState(10)
-  const [totalProducts, setTotalProducts] = useState(100)
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(false)
+  const { setFilterParams, totalProducts, loading, filteredProducts } = FilterStore()
+  const isNotMobile = useMediaQuery('(min-width: 768px)')
+
+  useEffect(() => {
+    setFilterParams({ page })
+  }, [page])
 
   return (
-    <div>
-      <div></div>
-      <Pagination page={1} pageSize={10} totalProducts={100} setPage={() => {}} />
+    <div className="flex flex-col">
+      {!isNotMobile && totalProducts > 12 ? (
+        <div className="border-b py-1 mt-[50px]">
+          <Pagination page={page} pageSize={12} totalProducts={totalProducts} setPage={setPage} />
+        </div>
+      ) : null}
+      <div className={twMerge('flex flex-col flex-1', totalProducts < 12 && 'mt-[50px] md:mt-0')}>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="grid grid-cols-1 min-[500px]:grid-cols-2 min-[1000px]:grid-cols-3 min-[1200px]:grid-cols-4 gap-8 md:gap-4 justify-items-center p-8 md:p-4">
+            {filteredProducts.map((p, index) => (
+              <ProductCard key={index} product={p} />
+            ))}
+          </div>
+        )}
+        {!loading && !totalProducts && (
+          <div className="flex-1 h-full w-full min-w-[375px] text-primary flex justify-center items-center">
+            <div className="text-center border-dotted border-4 border-primary px-10 py-10 rounded-full">
+              <p>პროდუქტი არ მოიძებნა</p>
+              <p>გთხოვთ სცადოთ</p>
+              <p>განსხვავებული ფილტრით</p>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="border-t">
+        <Pagination page={page} pageSize={12} totalProducts={totalProducts} setPage={setPage} />
+      </div>
     </div>
   )
 }
