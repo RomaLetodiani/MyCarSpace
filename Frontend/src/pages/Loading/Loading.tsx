2e0d5880b7b4983630f2210ff48c6d75edf95588
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-19 23:02:53 +0400

    Loading

---
+++ Frontend/src/pages/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="flex-1 h-full w-full min-w-[375px] text-primary flex justify-center items-center">
+      <div className="text-center border-dotted border-4 border-primary px-10 py-16 rounded-full">
+        <p>იტვირთება...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
