bc2f124589012f3ff66f7d99e2282c15c1711d45
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-22 17:15:24 +0400

    Bottom Footer

--- Frontend/src/Layout/BottomFooter.tsx
+++ Frontend/src/Layout/BottomFooter.tsx
@@ -1,5 +1,24 @@
+import { bottomFooterItems } from '../utils/BottomFooterItems'
+
 const BottomFooter = () => {
-  return <div>BottomFooter</div>
+  return (
+    <div className="max-w-[1440px] m-auto text-center">
+      <h2 className="text-2xl font-semibold">საუკეთესო სერვისი</h2>
+      <div className="grid place-items-center md:place-items-start grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 p-8 md:p-5">
+        {bottomFooterItems.map((item, index) => (
+          <div
+            key={index}
+            className="grid grid-cols-[80px_220px] md:grid-cols-[80px_1fr] items-center gap-5 rounded-xl p-5 bg-gradient-to-t from-oceanBlue/20 to-white/20 border text-center md:w-full shadow-inner"
+          >
+            <div className="w-20 h-20">
+              <img className="w-full h-full" src={item.image} alt="footer-item" />
+            </div>
+            <p className="font-bold">{item.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default BottomFooter
--- Frontend/src/utils/BottomFooterItems.ts
+++ Frontend/src/utils/BottomFooterItems.ts
@@ -3,14 +3,18 @@ import { cash, delivery, hours24, invoice } from '../assets'
 export const bottomFooterItems = [
   {
     image: hours24,
+    text: '24 საათიანი დახმარება',
   },
   {
     image: cash,
+    text: 'გადახდა მიწოდებისას',
   },
   {
     image: delivery,
+    text: 'უფასო მიწოდება თბილისში',
   },
   {
     image: invoice,
+    text: 'შეიძინე უსაფრთხოდ',
   },
 ]
