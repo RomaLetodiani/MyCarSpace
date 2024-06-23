e4dcdd293c74e7a5c525d1308a8c8fd027efeac8
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-23 15:13:22 +0400

    Products View

--- Frontend/src/pages/Home/Views/Products.tsx
+++ Frontend/src/pages/Home/Views/Products.tsx
@@ -1,23 +1,63 @@
+import { useRef } from 'react'
 import GlobalStore from '../../../Stores/Global.Store'
 import { logo } from '../../../assets'
 import Button from '../../../components/UI/Button'
+import useMediaQuery from '../../../hooks/useMediaQuery'
+import SliderArrow from '../../../components/SliderArrow'
+import { Link } from 'react-router-dom'
 
 const Products = () => {
   const { products } = GlobalStore()
+  const isNotMobile = useMediaQuery('(min-width: 768px)')
+  const scrollRef = useRef<null | HTMLDivElement>(null)
+
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: -500, // Adjust the scroll distance as needed
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: 500, // Adjust the scroll distance as needed
+        behavior: 'smooth',
+      })
+    }
+  }
   return products.length ? (
-    <div className="max-w-[1440px] m-auto p-5 my-8 grid text-center">
+    <div className="max-w-[1440px] m-auto md:p-5 my-8 text-center">
       <h2 className="text-2xl font-semibold">პროდუქტები</h2>
-      <div className="flex overflow-x-auto m-5 py-1 gap-5">
-        {products.map((product, index) => (
-          <div key={index} className="min-w-[200px] p-2 rounded-xl border">
-            <div className="">
-              <img src={product.imageUrls[0] ?? logo} alt={product.title} />
-            </div>
-            <p className="font-bold">{product.title}</p>
-          </div>
-        ))}
+      <div className="relative overflow-hidden md:px-10 p-5">
+        <div ref={scrollRef} className="flex overflow-x-auto py-2 gap-5">
+          {isNotMobile && (
+            <>
+              <SliderArrow direction="left" onClick={scrollLeft} />
+              <SliderArrow direction="right" onClick={scrollRight} />
+            </>
+          )}
+          {products.map((product, index) => (
+            <Link to={`/shop/${product._id}`}>
+              <div key={index} className="min-w-[200px] p-2 rounded-xl border shadow-sm">
+                <div className="">
+                  <img
+                    className="aspect-square"
+                    src={product.imageUrls[0] ?? logo}
+                    alt={product.title}
+                  />
+                </div>
+                <p className="font-bold">{product.title}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-      <Button className="place-self-center">მეტის ნახვა</Button>
+      <Link to={'/shop'}>
+        <Button className="place-self-center">მეტის ნახვა</Button>
+      </Link>
     </div>
   ) : null
 }
