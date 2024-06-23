24ed45001ff7411aa8ba3d58266c86ee610bae67
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-23 17:04:26 +0400

    Error Page

--- Admin/src/pages/Error/ErrorPage.tsx
+++ Admin/src/pages/Error/ErrorPage.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom'
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>
+  return (
+    <Link to={'/products'}>
+      <div className="h-screen w-full min-w-[375px] text-white bg-slate-200 flex justify-center items-center">
+        <div className="text-center bg-secondary/40 p-5 rounded-2xl">
+          <h1 className="text-4xl font-bold ">Error 404</h1>
+          <p className="text-2xl ">Page not found</p>
+        </div>
+      </div>
+    </Link>
+  )
 }
 
 export default ErrorPage
